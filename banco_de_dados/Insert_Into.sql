3ab75aed31bdfae20d491209c4fd8afe7270c53e
Breno Mendes <80074264+BrenoMendesMoura@users.noreply.github.com>
2021-11-27 14:56:58 -0300

    alterar nome

--- banco_de_dados/Insert_Into.sql
+++ banco_de_dados/Insert_Into.sql
@@ -1,11 +1,11 @@
 use dbos;
 
 INSERT INTO tbusuarios (iduser, nome_user, endereco_user, fone_user, email_user)
-values (1, 'Breno', 'Rua Monsenhor - 22','953497302',' bmoura.profissional@gmail.com');
+values (1, 'Breno', 'Rua Hiai - 12','999999999',' bmoura.profissional@gmail.com');
 
 INSERT INTO tbclientes (idclient, nome_cli, endereco_cli, fone_cli, email_cli)
-values (2, 'Rebecca', 'Naosei - 99','123456789',' Rebecca@gmail.com');
+values (2, 'Rebecca', 'Naosei - 99','123456789',' RR13a@gmail.com');
 INSERT INTO tbclientes (idclient, nome_cli, endereco_cli, fone_cli, email_cli)
-values (3, 'Giulia', 'Naosei - 88','231456789',' Giulia@gmail.com');
+values (3, 'Julia', 'Naosei - 88','231456789',' GFu2ia@gmail.com');
 
 select * from tbusuarios where nome_user = 'Breno'
